5de511aa867dca2dbaa56f02588dee3653c074f8
Chris Kurhan <chris.kurhan@dllgroup.com>
2022-04-01 13:00:14 -0500

    Initial commit

---
+++ webapi-starter/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using webapi_starter.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi_starter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> Users = new List<User>()
+        {
+            new User(0, "Jorn"),
+            new User(3, "Markus"),
+            new User(2, "Andrew"),
+            new User(4, "Ori"),
+            new User(1, "Mike")
+        };
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return Users;
+        }
+    }
+}
---
+++ webapi-starter/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace webapi_starter.Models
+{
+    [Serializable]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public User(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
